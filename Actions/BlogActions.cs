33f03870a1dae9aeabde987df1a7fcf08746f293
karani-ken <academice79@gmail.com>
2023-08-15 00:15:11 +0300

    added fetch and insert into db functions

---
+++ Actions/BlogActions.cs
@@ -0,0 +1,35 @@
+﻿using EFGetStarted.Database;
+using EFGetStarted.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFGetStarted.Actions
+{
+    public class BlogActions
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+
+        public void addBlog()
+        {
+            
+            Console.WriteLine("Enter a new Blog.");
+            string name = Console.ReadLine();
+            //db.Add(new Blog { BlogName = "http://blogs.msdn.com/adonet" });
+            db.Add(new Blog { BlogName = name});
+            db.SaveChanges();
+            Console.WriteLine("added successfully");
+        }
+        public void getBlogs()
+        {
+            var blogs = db.Blogs.ToList();
+            foreach(var blog in blogs)
+            {
+                Console.WriteLine($"BlogId:{blog.BlogId}\t BlogName:{blog.BlogName}");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
-﻿using EFGetStarted.Database;
+﻿using EFGetStarted.Actions;
+using EFGetStarted.Database;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 ApplicationDbContext applicationDbContext = new ApplicationDbContext();
-
+BlogActions blog = new BlogActions();
+blog.addBlog();
+blog.getBlogs();
 
